819fe45cabc418e870fa63365314227d2d12cdbb
henrikkyhl <henrikkyhl@hotmail.com>
2022-09-08 18:10:48 +0200

    Refactored to use top-level statements

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,26 +1,72 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using TodoApi.Data;
+using TodoApi.Models;
 
-namespace TodoApi
-{
-    public class Program
-    {
-        public static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// SqLite database:
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlite("Data Source=TodoDb.db"));
+// Register SqLite database initializer for dependency injection.
+builder.Services.AddTransient<IDbInitializer, SqLiteDbInitializer>();
+
+// Register TodoItem repository for dependency injection.
+builder.Services.AddScoped<IRepository<TodoItem>, TodoItemRepository>();
+
+// Configure the default CORS policy.
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(
+        builder =>
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        })
+);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// Register the Swagger generator using Swashbuckle.
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "A simple example ASP.NET Core Web API"
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // Initialize the database.
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetService<TodoContext>();
+        var dbInitializer = services.GetService<IDbInitializer>();
+        dbInitializer.Initialize(dbContext);
     }
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- TodoClient/Program.cs
+++ TodoClient/Program.cs
@@ -1,51 +1,42 @@
-﻿using System;
+﻿using TodoClient;
 
-namespace TodoClient
+Console.WriteLine("Create a new TodoItem:");
+
+var newItem = new TodoItem
 {
-    class Program
+    Name = "Test",
+    IsComplete = true
+};
+
+bool created = new ServiceGateway().CreateItem(newItem);
+if (created)
+    Console.WriteLine("A new TodiItem was succesfully created!");
+else
+    Console.WriteLine("A new TodiItem could not be created!");
+
+Console.WriteLine("Display all TodoItems:");
+var items = new ServiceGateway().GetItems();
+foreach (var item in items)
+    Console.WriteLine(item.Id + ": " + item.Name + ", IsComplete: " + item.IsComplete);
+
+if (created)
+{
+    Console.WriteLine("Update the newly created item:");
+    newItem.Name = "Test updated";
+    bool updated = new ServiceGateway().UpdateItem(newItem);
+    if (updated)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Create a new TodoItem:");
-
-            var newItem = new TodoItem
-            {
-                Name = "Test",
-                IsComplete = true
-            };
-
-            bool created = new ServiceGateway().CreateItem(newItem);
-            if (created)
-                Console.WriteLine("A new TodiItem was succesfully created!");
-            else
-                Console.WriteLine("A new TodiItem could not be created!");
-
-            Console.WriteLine("Display all TodoItems:");
-            var items = new ServiceGateway().GetItems();
-            foreach (var item in items)
-                Console.WriteLine(item.Id + ": " + item.Name + ", IsComplete: " + item.IsComplete);
-
-            if (created)
-            {
-                Console.WriteLine("Update the newly created item:");
-                newItem.Name = "Test updated";
-                bool updated = new ServiceGateway().UpdateItem(newItem);
-                if (updated)
-                {
-                    Console.WriteLine("Display the updated TodoItem:");
-                    var updatedItem = new ServiceGateway().GetItem(newItem.Id);
-                    Console.WriteLine(updatedItem.Id + ": " + updatedItem.Name + ", IsComplete: " + updatedItem.IsComplete);
-                }
-                else
-                    Console.WriteLine("The TodiItem could not be updated!");
-
-                Console.WriteLine("Delete the newly created item:");
-                bool deleted = new ServiceGateway().DeleteItem(newItem.Id);
-                if (deleted)
-                    Console.WriteLine("The new TodiItem was succesfully deleted!");
-                else
-                    Console.WriteLine("The new TodiItem could not be deleted!");
-            }
-        }
+        Console.WriteLine("Display the updated TodoItem:");
+        var updatedItem = new ServiceGateway().GetItem(newItem.Id);
+        Console.WriteLine(updatedItem.Id + ": " + updatedItem.Name + ", IsComplete: " + updatedItem.IsComplete);
     }
+    else
+        Console.WriteLine("The TodiItem could not be updated!");
+
+    Console.WriteLine("Delete the newly created item:");
+    bool deleted = new ServiceGateway().DeleteItem(newItem.Id);
+    if (deleted)
+        Console.WriteLine("The new TodiItem was succesfully deleted!");
+    else
+        Console.WriteLine("The new TodiItem could not be deleted!");
 }
